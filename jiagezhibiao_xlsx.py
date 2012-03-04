5ebbaab557242b74d43e16d1463aed32f7a1adb0
Feng Qin <phcho99@163.com>
2012-03-04 18:58:29 +0800

    change note

--- jiagezhibiao_xlsx.py
+++ jiagezhibiao_xlsx.py
@@ -8,12 +8,16 @@ from goods import Goods
 def inputP0():   
     wb=load_workbook(filename = 'data//quanguo.xlsx', use_iterators=True)
     sheet_names= wb.get_sheet_names()
-    ws=wb.get_sheet_by_name(sheet_names[0])     #数据表必须是数据文件第一个表
+    #数据表必须是数据文件第一个表
+    ws=wb.get_sheet_by_name(sheet_names[0])     
     d={}
     for row in ws.iter_rows(row_offset=1,):
-        tmp=row[4].internal_value               #读入全国平均单价
-        result=Goods(0,0,tmp)                   #创建聚合的价格数据
-        d[str(row[2].internal_value)[0:8]]=result    #与8位税号建立关联关系 
+        #读入全国平均单价
+        tmp=row[4].internal_value 
+        #创建聚合的价格数据              
+        result=Goods(0,0,tmp)
+        #与8位税号建立关联关系                    
+        d[str(row[2].internal_value)[0:8]]=result    
     return d
 
 #输入通关数据
@@ -27,8 +31,10 @@ def inputP1(d):
             sheet_names=wb.get_sheet_names()
             ws=wb.get_sheet_by_name(sheet_names[0])          
             for row in ws.iter_rows(row_offset=1,):
-                hscode=unicode(row[14].internal_value)[0:8]             #不同数据文件需要调整行号
-                if hscode in d:              #如果全国平均单价没有此类，则舍弃该数据
+                #不同数据文件需要调整行号
+                hscode=unicode(row[14].internal_value)[0:8]
+                #如果全国平均单价没有此类，则舍弃该数据             
+                if hscode in d:              
                     d[hscode].ttl+=float(row[25].internal_value)
                     d[hscode].amout+=float(row[28].internal_value)
                     d[hscode].huilv=float(row[19].internal_value)                
@@ -51,9 +57,11 @@ def calc(d):
     ws.append(['HS_code','影响度'])    
     for k,v in d.iteritems():
         v.setP1()
-        if flag(v):                         #引入排除奇异点函数
+        #引入排除奇异点函数
+        if flag(v):                         
             fenzi+=v.p1*v.ttl
             fenmu+=v.p0*v.ttl
+    #计算每个税号的影响度
     for k,v in d.iteritems():
         if flag(v):
             v.affect=v.ttl*(v.p1-v.p0)/fenmu*100.0
